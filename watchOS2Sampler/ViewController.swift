bc60fa3919d67216e0f794c541c2d680ae037b12
shu223 <shuichi0526@gmail.com>
2015-06-10 14:22:13 -0700

    Initial Commit

---
+++ watchOS2Sampler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchOS2Sampler
+//
+//  Created by Shuichi Tsutsumi on 2015/06/10.
+//  Copyright © 2015年 Shuichi Tsutsumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
